547fff365ef41b9234b3bb216d91abbc9c854981
RAHUL <rahulbairwabhl@gmail.com>
2020-05-26 01:02:30 +0530

    third change

--- project.py
+++ project.py
@@ -78,8 +78,8 @@ train_datagen=ImageDataGenerator(
       fill_mode='nearest')
 validation_datagen = ImageDataGenerator(rescale=1./255)
 
-train_data_dir = '../cell_images/cell_images/'
-validation_data_dir = '../cell_images/valid/'
+train_data_dir = './cell_images/'
+validation_data_dir = './valid/'
 train_batchsize = 10
 val_batchsize = 8
 train_generator=train_datagen.flow_from_directory(train_data_dir,
